c682f2a063bd1efaa9cfb0b25e6321cc91976af5
mk99995 <mejkorte@gmail.com>
2022-11-17 13:57:57 +0200

    more tests

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,21 +1,46 @@
 const blogsRouter = require("express").Router();
+const app = require("../app");
+// const { collection } = require("../models/blog");
 const Blog = require("../models/blog");
 
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({});
-  response.json(blog); //kirjanmerkki
+  response.json(blogs);
 
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
+  //   Blog.find({}).then((blogs) => {
+  //     response.json(blogs);
+  //   });
 });
 
-blogsRouter.post("/", (request, response) => {
-  console.log("asd");
-  console.log(request.body.likes);
-  const blog = new Blog(request.body);
+blogsRouter.delete(`/:id`, async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id);
+  response.status(204).end();
+});
 
-  blog.save().then((result) => {
+blogsRouter.put("/:id", (request, response, next) => {
+  Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    .then((updatedBlog) => {
+      response.json(updatedBlog);
+    })
+    .catch((error) => next(error));
+});
+
+blogsRouter.post("/", async (request, response) => {
+  const blog = new Blog(request.body);
+  console.log(blog.title === undefined);
+  if (
+    blog.title === undefined ||
+    blog.author === undefined ||
+    blog.url === undefined
+  ) {
+    return response.status(400).json({ error: "content missing" });
+  }
+
+  if (blog.likes === undefined) {
+    blog.likes = 0;
+  }
+
+  await blog.save().then((result) => {
     response.status(201).json(result);
   });
 });
--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
+const { insertMany } = require("../models/blog");
 const api = supertest(app);
 const Blog = require("../models/blog");
 
@@ -41,10 +42,7 @@ const initialBlogs = [
 
 beforeEach(async () => {
   await Blog.deleteMany({});
-  for (let i = 0; i < initialBlogs.length; i++) {
-    let blogObject = new Blog(initialBlogs[i]);
-    await blogObject.save();
-  }
+  await Blog.insertMany(initialBlogs);
 });
 
 describe("gets", () => {
@@ -55,7 +53,7 @@ describe("gets", () => {
       .expect("Content-Type", /application\/json/);
   });
 
-  test("there are two blogs", async () => {
+  test("there is correct amount of blogs", async () => {
     const response = await api.get("/api/blogs");
 
     expect(response.body).toHaveLength(initialBlogs.length);
@@ -69,6 +67,84 @@ describe("gets", () => {
     expect(contents).toContain("asd");
   });
 });
+
+describe("posts", () => {
+  test("a valid blog can be added", async () => {
+    const newBlog = {
+      title: "zxcl",
+      author: "Ed Di",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 0,
+    };
+
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    const response = await api.get("/api/blogs");
+    const titles = response.body.map((r) => r.title);
+
+    expect(response.body).toHaveLength(initialBlogs.length + 1);
+    expect(titles).toContain("zxcl");
+  });
+
+  test("blog without title is not added", async () => {
+    const newBlog = {
+      _id: "5a422aa71b54a676234d17f8",
+      //title: "dasds",
+      //author: "Edsger W. Dijkstra",
+      //url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0,
+    };
+    await api.post("/api/blogs").send(newBlog).expect(400);
+    const response = await api.get("/api/blogs");
+    expect(response.body).toHaveLength(initialBlogs.length);
+  });
+
+  test("blog without likes has 0 likes", async () => {
+    const newBlog = {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "dasds",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      //likes: 5,
+      __v: 0,
+    };
+    await api.post("/api/blogs").send(newBlog).expect(201);
+    const response = await api.get("/api/blogs");
+    expect(response.body[response.body.length - 1].likes).toBe(0);
+  });
+});
+
+describe("put", () => {
+  test("blog can be modified", async () => {
+    const blogs = await Blog.find({});
+    let blogsd = blogs.map((blog) => blog.toJSON());
+
+    liked = blogsd[0];
+
+    await api.put(`/api/blogs/${liked.id}`).expect(204);
+  });
+});
+
+describe("delete", () => {
+  test("blog can be deleted", async () => {
+    const blogs = await Blog.find({});
+    let blogsd = blogs.map((blog) => blog.toJSON());
+
+    deleted = blogsd[0];
+
+    await api.delete(`/api/blogs/${deleted.id}`).expect(204);
+    blogsAfter = await Blog.find({});
+    blogsAfter = blogsAfter.map((blog) => blog.toJSON());
+
+    expect(blogsAfter).toHaveLength(blogsd.length - 1);
+  });
+});
+
 afterAll(() => {
   mongoose.connection.close();
 });
